5a4442da03e1863d86663c04c163478076e88bf8
kabiru <mwangikabiru21@gmail.com>
2022-10-14 01:38:39 +0300

    Did you know you could do this

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"net/http"
-	"time"
 	"github.com/go-resty/resty/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/tidwall/gjson"
+	"net/http"
+	"time"
 )
 
 type BaseResponse struct {
@@ -19,7 +19,7 @@ func PerformAPICall(c *fiber.Ctx) error {
 	client.SetDebug(true)
 	client.SetContentLength(true)
 	client.SetTimeout(time.Duration(time.Duration(60) * time.Second))
-	
+
 	response, err := client.R().
 		Get("https://random-data-api.com/api/v2/users")
 
@@ -47,11 +47,11 @@ func PerformAPICall(c *fiber.Ctx) error {
 }
 
 func main() {
-    app := fiber.New()
-  
-    app.Get("/random-data", func(c *fiber.Ctx) error {
-        return PerformAPICall(c);
-    })
-  
-    app.Listen(":6000")
+	app := fiber.New()
+
+	app.Get("/random-data", func(c *fiber.Ctx) error {
+		return PerformAPICall(c)
+	})
+
+	app.Listen(":6000")
 }
